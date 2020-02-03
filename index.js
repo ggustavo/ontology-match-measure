05e4586f6be004d4a91e43ae9bb6d68010abf884
Gustavo Moraes <gustavomoraes94@gmail.com>
2020-02-03 18:55:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,26 +6,54 @@ var parser = require('fast-xml-parser');
 const pathReference = 'datasets/ontofarm/references/cmt-conference.rdf';
 const pathTool = 'datasets/ontofarm/tools/ALIN-cmt-conference.rdf';
 
-//const refMatches = computeReference(pathReference);
-const toolMatches = computeTool(pathTool);
 
+console.log("\n\n--------------- Reference Matches --> " + pathReference);
+const refMatches = computeMatches(pathReference);
+console.log("Total: " + refMatches.length);
 
+console.log("\n\n--------------- Tool Matches --> " + pathTool);
+const toolMatches = computeMatches(pathTool);
+console.log("Total: " + toolMatches.length);
 
+console.log("\n\n--------------- Intersect Matches --> " + pathTool);
+const intersectMatches = intersect(refMatches,toolMatches);
+console.log("Total: " + intersectMatches.length);
 
+console.log("\n\n--------------- Measures --> " + pathTool);
+calculeMeasures(refMatches,toolMatches,intersectMatches);
 
 
 /*
     ---------------- Functions ----------------
 */
 
-function computeTool(path){
 
-    console.log(RDFtoJson(path));
+function calculeMeasures(refMatches, toolMatches, intersect){
+    const precision =  intersect.length / toolMatches.length;
+    const recall = intersect.length / refMatches.length;
+    const f1 = 2 * ( (precision * recall) / (precision + recall) )
+    console.log("Precision: " +  precision );
+    console.log("Recal: " + recall);
+    console.log("F1: " +  f1);
 }
 
-function computeReference(path){
-    console.log();
-    console.log("--------------- Reference Matches ---------------");
+function intersect(refMatches, toolMatches){
+    const matches = [];
+    for(let i = 0; i < refMatches.length; i++){
+        for(let j = 0; j < toolMatches.length; j++){
+            if( refMatches[i].entity1 === toolMatches[j].entity1 && 
+                refMatches[i].entity2 === toolMatches[j].entity2){
+                    printMatch(refMatches[i]);
+                    matches.push(refMatches[i]);
+            }
+        }    
+    }    
+    
+    return matches;
+
+}
+
+function computeMatches(path){
     const matchesRDF = RDFtoJson(path)['rdf:RDF']['Alignment']["map"];
     const matches = [];
     for(let i = 0; i < matchesRDF.length; i++){
